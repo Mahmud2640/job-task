b307317687b84df57f6ea6f25260e51f162473d7
Mahmud2640 <mdmahmud484@gmail.com>
2022-06-29 00:47:36 +0600

    final

--- src/App.js
+++ src/App.js
@@ -12,39 +12,36 @@ import "./App.css";
 import { OrderDetails } from "./data";
 
 function App() {
-  // let SSObj: SpreadsheetComponent;
-  // const Button = () => {
-  //   SSObj.save({
-  //     url: "https://ej2services.syncfusion.com/production/web-services/api/Spreadsheet/Save",
-  //     fileName: "Sample",
-  //     saveType: "Csv",
-  //   });
-  // };
   return (
-    <div className="App">
-      {/* <button onClick={Button}>Save</button> */}
-      <SpreadsheetComponent
-        allowOpen={true}
-        allowSave={true}
-        saveUrl="https://ej2services.syncfusion.com/production/web-services/api/spreadsheet/save"
-      >
-        <SheetsDirective>
-          <SheetDirective>
-            <RangesDirective>
-              <RangeDirective dataSource={OrderDetails}></RangeDirective>
-            </RangesDirective>
-            <ColumnsDirective>
-              <ColumnDirective width={120}></ColumnDirective>
-              <ColumnDirective width={110}></ColumnDirective>
-              <ColumnDirective width={100}></ColumnDirective>
-              <ColumnDirective width={180}></ColumnDirective>
-              <ColumnDirective width={130}></ColumnDirective>
-              <ColumnDirective width={130}></ColumnDirective>
-            </ColumnsDirective>
-          </SheetDirective>
-        </SheetsDirective>
-      </SpreadsheetComponent>
-    </div>
+    <>
+      <div className="title">
+        <h2>Fournir Innovations Pvt Ltd.</h2>
+      </div>
+      <div className="App">
+        <SpreadsheetComponent
+          allowOpen={true}
+          allowSave={true}
+          saveUrl="https://ej2services.syncfusion.com/production/web-services/api/spreadsheet/save"
+        >
+          <SheetsDirective>
+            <SheetDirective>
+              <RangesDirective>
+                <RangeDirective dataSource={OrderDetails}></RangeDirective>
+              </RangesDirective>
+              <ColumnsDirective>
+                <ColumnDirective width={120}></ColumnDirective>
+                <ColumnDirective width={110}></ColumnDirective>
+                <ColumnDirective width={100}></ColumnDirective>
+                <ColumnDirective width={100}></ColumnDirective>
+                <ColumnDirective width={100}></ColumnDirective>
+                <ColumnDirective width={130}></ColumnDirective>
+                <ColumnDirective width={150}></ColumnDirective>
+              </ColumnsDirective>
+            </SheetDirective>
+          </SheetsDirective>
+        </SpreadsheetComponent>
+      </div>
+    </>
   );
 }
 
